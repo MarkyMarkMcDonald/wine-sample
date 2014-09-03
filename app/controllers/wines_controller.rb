1cd2594b2ae81a1539d9b045b385bf059ab2e2f6
Mark McDonald <mark.mcdonald.ga@gmail.com>
2014-09-02 22:04:58 -0400

    Remove active record

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -3,8 +3,7 @@ class WinesController < ApplicationController
     wine_names = params["wine-list"].split("\n").map(&:strip)
     @wines = wine_names.map do |name|
       response = HTTParty.get("http://services.wine.com/api/beta2/service.svc/json/catalog?apikey=cae7f4e3edbe3608d44bdf0c1280af9b", {query: {term: name}})
-      JSON.parse(response.body)
+      JSON.parse(response.body)['Products']['List']
     end
-    puts @wines
   end
 end
