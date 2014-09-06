520e313384e9225f52fcc932da9dcb6b45b0139d
Mark McDonald <pair+mmcdonald@pivotallabs.com>
2014-09-06 16:11:29 -0400

    Display wine information in a table

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -1,9 +1,23 @@
 class WinesController < ApplicationController
   def create
     wine_names = params["wine-list"].split("\n").map(&:strip)
-    @wines = wine_names.map do |name|
+    @wine_search_results = wine_names.map do |name|
       response = HTTParty.get("http://services.wine.com/api/beta2/service.svc/json/catalog?apikey=cae7f4e3edbe3608d44bdf0c1280af9b", {query: {term: name}})
       JSON.parse(response.body)['Products']['List']
     end
+
+    @wine_search_results.map! do |result_list|
+      result_list.first
+    end
+
+
+    @wine_search_results.map! do |result|
+      {
+        name: result['Name'],
+        retail_price: result['PriceRetail'],
+        expert_rating: result['Ratings'].try(:[], 'HighestScore'),
+      }
+    end
+
   end
 end
